e8123519c6ed72af63f77f23ada08709981a1f03
BakerIbuypower <BakerIbuypower@IbuyBaker>
2022-01-06 11:55:13 -0500

    Day11 Hw Q3

---
+++ src/methods/HwQ3.java
@@ -0,0 +1,26 @@
+package methods;
+
+public class HwQ3 {
+	public static void main(String[] args) {
+
+		int myTotal = sumDouble(2, 2);
+		System.out.println(myTotal);
+
+	}
+
+	public static int sumDouble(int valueOne, int valueTwo) {
+
+		int result = 0;
+
+		if (valueOne == valueTwo) {
+			result = (valueOne + valueTwo) * 2;
+
+		} else {
+			result = valueOne + valueTwo;
+
+		}
+
+		return result;
+	}
+
+}
